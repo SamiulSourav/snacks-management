dc0cfb91daff455d93dead7a673c5df1463eb8aa
zubairrafi <walleeva2018@gmail.com>
2025-07-04 00:46:19 +0600

    Food Item updated
    Signed-off-by: zubairrafi <walleeva2018@gmail.com>

--- src/utils/foodItem.ts
+++ src/utils/foodItem.ts
@@ -1,57 +1,38 @@
 export const foodItems = [
   {
-    name: 'Tundol ruti',
-    bnName: 'তুন্দল রুটি ',
-    id: 'mugdal',
-    cost: 15,
-  },
-  {
-    name: 'Mug dal',
-    bnName: 'মুগ ডাল',
-    id: 'mugdalvaji',
-    cost: 30,
-  },
-  {
-    name: 'Dal',
-    bnName: 'ডাল',
-    id: 'buterdal',
+    name: 'Tundul Ruti',
+    bnName: 'তুন্দুল রুটি',
+    id: 'tundul_ruti',
     cost: 20,
-
+  },
+  {
+    name: 'Normal Porota',
+    bnName: 'নরমাল পরোটা',
+    id: 'normal_porota',
+    cost: 20,
+  },
+  {
+    name: 'Mug Dal',
+    bnName: 'মুগের ডাল',
+    id: 'muger_dal',
+    cost: 50,
+  },
+  {
+    name: 'Buter Dal',
+    bnName: 'বুটের ডাল',
+    id: 'buter_dal',
+    cost: 40,
   },
   {
     name: 'Bhaji',
     bnName: 'ভাজি',
     id: 'bhaji',
-    cost: 20,
-  },
-  {
-    name: 'Porata',
-    bnName: 'পরাটা',
-    id: 'porata',
-    cost: 15,
-  },
-  {
-    name: 'Tel Chara Porata',
-    bnName: 'তেল ছাড়া পরাটা',
-    id: 'telcharaporata',
-    cost: 15,
-  },
-  {
-    name: 'Dim Poach ',
-    bnName: 'ডিম পোঁচ',
-    id: 'dimpoc',
-    cost: 25,
-  },
-  {
-    name: 'Dim Vaji',
-    bnName: 'ডিম ভাজি',
-    id: 'dimvaji',
-    cost: 25,
-  },
-  {
-    name: 'Shahi Halua',
-    bnName: 'শাহি হালুয়া ',
-    id: 'halua',
     cost: 40,
+  },
+  {
+    name: 'Chicken Khichuri',
+    bnName: 'চিকেন খিচুড়ি',
+    id: 'chicken_khichuri',
+    cost: 260,
   },
 ]
